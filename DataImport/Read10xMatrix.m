cb56f8d7e8e43682af6d8a1f9a115923fa5178f5
Johan Gustafsson <gustajo@chalmers.se>
2019-06-13 14:50:36 +0200

    feat: Added some optional parameter to specify filenames in Read10xMatrix.

--- DataImport/Read10xMatrix.m
+++ DataImport/Read10xMatrix.m
@@ -1,5 +1,6 @@
 %Reads 10x files into a SCDataset, 
-function ds = Read10xMatrix(directoryPath)
+function ds = Read10xMatrix(directoryPath, matrixFilename, genesFilename, ...
+                            barcodesFilename, barcodesHeaders, genesColumn)
 % Read10xMatrix
 %   Reads the standard output single-cell data from 10x Genomics into an
 %   SCDataset
@@ -8,16 +9,42 @@ function ds = Read10xMatrix(directoryPath)
 %   directoryPath    Path to the directory in which the 10x files are,
 %                    i.e. the files  'matrix.mtx', 'genes.tsv' and 
 %                    'barcodes.tsv'. No slash at the end!
+%                    The barcodes file may contain multiple columns as long
+%                    as the barcodes are in the first!
+%   matrixFilename   (Optional) Matrix filename, defaults to 'matrix.mtx'
+%   genesFilename    (Optional) Genes filename, defaults to 'matrix.mtx'
+%   barcodesFilename (Optional) Barcodes filename, defaults to 'matrix.mtx'
+%   barcodesHeaders  (Optional) If the barcodes file contains headers,
+%                    defaults to false
+%   genesColumn      (Optional) Column for the genes in the genes file,
+%                    defaults to 2
 %
 % Usage: ds = Read10xMatrix('../../ImportableData/SomePath')
 %
-% Johan Gustafsson, 2019-05-20
+% Johan Gustafsson, 2019-06-02
 %
 
+if nargin < 6
+     genesColumn = 2;
+end
+if nargin < 5
+     barcodesHeaders = false;
+end
+if nargin < 4
+     barcodesFilename = 'barcodes.tsv';
+end
+if nargin < 3
+     genesFilename = 'genes.tsv';
+end
+if nargin < 2
+     matrixFilename = 'matrix.mtx';
+end
 
-matrixPath = strcat(directoryPath,'/matrix.mtx');
-genesPath = strcat(directoryPath,'/genes.tsv');
-barcodesPath = strcat(directoryPath,'/barcodes.tsv');
+
+
+matrixPath = strcat(directoryPath, '/', matrixFilename);
+genesPath = strcat(directoryPath, '/', genesFilename);
+barcodesPath = strcat(directoryPath, '/', barcodesFilename);
 
 ds = SCDataset;
 
@@ -55,7 +82,7 @@ ds.data = spconvert(A);
 fclose(fileID);
 
 f = readtable(genesPath, 'ReadVariableNames',false, 'ReadRowNames', false, 'Delimiter', '\t', 'FileType', 'text');
-ds.genes = table2cell(f(:, 2));
+ds.genes = table2cell(f(:, genesColumn));
 
 %fill out the last few genes with zeros, they are not part of the matrix
 [matrows,cols] = size(ds.data);
@@ -64,7 +91,7 @@ if (matrows < generows)
     ds.data = [ds.data;zeros(generows-matrows,cols)];
 end
 
-f = readtable(barcodesPath, 'ReadVariableNames',false, 'ReadRowNames', false, 'Delimiter', '\t', 'FileType', 'text');
+f = readtable(barcodesPath, 'ReadVariableNames', barcodesHeaders, 'ReadRowNames', false, 'Delimiter', '\t', 'FileType', 'text');
 ds.cellIds = table2cell(f(:, 1)).';
 
 ds = ds.fillEmpties();
